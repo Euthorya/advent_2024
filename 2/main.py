5f37762760e5f0b15bfbafef01718cdbc5e6aa13
Mikhail Efimov <efimovhq@gmail.com>
2025-02-17 16:11:52 +0400

    Part 1 solution

--- 2/main.py
+++ 2/main.py
@@ -5,42 +5,23 @@ def read_from_file(file):
         for line in f:
             yield line
 
-
 def reducer(asc):
-    #asc = asc
     def wrapper(x, y):
         if x is None or y is None:
             return None
         x, y = int(x), int(y)
         return y if 0 < abs(x-y) < 4 and (x < y) == asc else None
     return wrapper
-        
-
-
-
-
 
 def main():
-    a = [i for i in read_from_file("test.txt")]
     result = 0
     valid_lines = []
-    for i in a:
-        # numbers = i.split()
-        # valid = reduce(reducer(numbers[0] < numbers[1]), numbers)
-        # if valid:
-        #     result += 1
-        #     valid_lines.append(i)
+    for i in read_from_file("test.txt"):
         numbers = i.split()
-        asc = numbers[0] < numbers[1]
-        valid = True
-        for j in range(len(numbers)-1):
-            if not (0< abs(int(numbers[j+1]) - int(numbers[j])) < 4) or (int(numbers[j]) < int(numbers[j+1]))!= asc:
-                valid = False
-                break
+        valid = reduce(reducer(int(numbers[0]) < int(numbers[1])), numbers)
         if valid:
             result += 1
-                
-
+            valid_lines.append(i)
     print(result)
 
 
