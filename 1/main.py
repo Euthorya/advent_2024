ec66788a01d182c9357188ce9b633b9f6a2ba46e
Mikhail Efimov <efimovhq@gmail.com>
2025-02-17 14:44:39 +0400

    Solution for part 2

--- 1/main.py
+++ 1/main.py
@@ -7,10 +7,15 @@ def read_from_file(file):
 def main():
     a, b = [], []
     for i in read_from_file("test.txt"):
-        a.append(i[0])
-        b.append(i[1])
+        a.append(int(i[0]))
+        b.append(int(i[1]))
     a.sort()
     b.sort()
-    print(sum(abs(int(i)-int(j)) for i, j in zip(a,b))) 
+    print(sum(abs(i-j) for i, j in zip(a,b)))
+
+    m = {}
+    for i in b:
+        m[i] = m.get(i, 0) + 1
+    print(sum(i * m[i] for i in a if i in m))
 
 main()
